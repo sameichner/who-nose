bd6ae78e8b9bfc8e5ae404c3a928dfb69824d305
sameichner <sameichner@gmail.com>
2019-01-30 21:00:13 -0500

    cooler

--- deploy.py
+++ deploy.py
@@ -1,7 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
+
 @app.route("/")
 def home():
-   return "Hello, World!"
+   return render_template("index.html")
+
+@app.route("/tester.html")
+def tester():
+    return "test success"
+
 if __name__ == "__main__":
    app.run(debug=True)
