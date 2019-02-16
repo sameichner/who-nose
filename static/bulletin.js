9ba41e7cf117831f621bf086352f833345ae165b
sameichner <sameichner@gmail.com>
2019-02-15 21:22:56 -0500

    the bulletin lets you post now

--- static/bulletin.js
+++ static/bulletin.js
@@ -1,11 +1,25 @@
 //to be done later
 alert("javascript is connected successfully");
+
 document.getElementById("newpostsubmit").onclick = function () {
     var name = document.getElementById("newpostname").value;
     var dt = Date();
     var content = document.getElementById("newpostbody").value;
     post(name,dt.toLocaleString(),content);
 };
+
+document.getElementById("newpostcancel").onclick = function () {
+    document.getElementById("newpostname").value = "";
+    document.getElementById("newpostbody").value = "";
+};
+
 function post(name,time,content){
-    alert(name + "and" + time + " and" + content);
+    //alert(name + "and" + time + " and" + content);
+
+    temp = document.getElementsByTagName("template")[0];
+    temp.content.getElementById("posttitle").textContent = name;
+    temp.content.getElementById("postcontent").textContent = content;
+    temp.content.getElementById("posttime").title = time;
+
+    document.getElementById("board").appendChild(temp.content.querySelector("div").cloneNode(true), true);
 }
